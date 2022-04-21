46fb4bb1134e401d30b0c5015533cb9bb35cc504
Emiliano Acevedo <emi.acevedo.letras@gmail.com>
2022-04-21 11:14:20 -0300

    add

--- src/main/java/org/ejercicio/Main.java
+++ src/main/java/org/ejercicio/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.ejercicio;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(agregarIva(70));
+
+    }
+
+
+    public static double agregarIva(double precio){
+
+            double iva = 21;
+
+            return precio + ((precio/100) * iva);
     }
 }
